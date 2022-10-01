df259fd168c38f927842608295ab81922a4fcf94
Vladyslav <vladyslav36@gmail.com>
2022-10-01 15:59:19 +0300

    ыусщтв

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(cors())
  
+
 app.use('/',(req,res)=>res.json({msg:'hello'}))
 
 app.listen(port,console.log(`Server running on port ${port} in ${process.env.NODE_MODE} mode`))
